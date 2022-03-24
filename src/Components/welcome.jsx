cc12b3f59a66e768495f2a14237d94b202a92d2e
Pedro <pedroagustinpeyon@gmail.com>
2022-03-24 16:07:15 -0300

    patch

--- src/Components/welcome.jsx
+++ src/Components/welcome.jsx
@@ -7,12 +7,27 @@ export default function Welcome(){
     return(
         <div>
             <h1>Tic Tac Toe</h1>
+            <h4>Created by Pedro Peyon</h4>
+            <div className='divButtons'>
+                <div>
+                    <Link className='link' to="/offline"><button className='buttton'>Offline</button></Link>
+                    <Link className='link' to="/online" ><button className='buttton'>Online</button></Link>
+                </div>
             
-            <Link className='link' to="/offline"> <button>Offline</button></Link>
-            <Link className='link' to="/online"> <button>Online</button></Link>
+            <Link className='link' to="/"       ><button className='butttonA'>Instructions</button></Link>
+            <br/>
+            <br/>
+
+            <div>
+                <a target="_blank" href='https://pedro-peyon-portafolio.vercel.app/'><button className='buttton'>Front code</button></a>
+                <a target="_blank" href='https://github.com/Aguxteen/tic-tac-toe-server'><button className='buttton'>Back code</button></a>
+            </div>
+                <a target="_blank" href='https://github.com/Aguxteen/tic-tac-toe'><button className='buttton'>Creator</button></a>
+
             
-            <h2>Created by <a target="_blank" href='https://pedro-peyon-portafolio.vercel.app/'>Pedro Peyon</a></h2>
-            <h2>Code  <a target="_blank" href='https://github.com/Aguxteen/tic-tac-toe'>here</a></h2>
+
+            </div>
+            
         </div>
     )
 }
