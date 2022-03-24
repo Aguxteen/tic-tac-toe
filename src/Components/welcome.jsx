b9282803080e45feb1333d6336d57e8a287a1eff
Pedro <pedroagustinpeyon@gmail.com>
2022-03-24 15:07:22 -0300

    patch

--- src/Components/welcome.jsx
+++ src/Components/welcome.jsx
@@ -7,9 +7,14 @@ export default function Welcome(){
     return(
         <div>
             <h1>Tic Tac Toe</h1>
-            <Link to="/offline"> <button>Offline</button></Link>
-            <Link to="/online"> <button>Online</button></Link>
-
+            
+            <Link className='link' to="/offline"> <button>Offline</button></Link>
+            <Link className='link' to="/online"> <button>Online</button></Link>
+            
+            <h2>Created by <a target="_blank" href='https://pedro-peyon-portafolio.vercel.app/'>Pedro Peyon</a></h2>
+            <h2>Code  <a target="_blank" href='https://github.com/Aguxteen/tic-tac-toe'>here</a></h2>
         </div>
     )
 }
+
+
